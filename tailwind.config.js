08a0929e8e40ba61b29df9fb5126720c713b03d9
Youssef Albokl <88749793+Boklino@users.noreply.github.com>
2023-02-08 19:07:49 +0200

    First commit to the portfolio

---
+++ tailwind.config.js
@@ -0,0 +1,24 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  darkMode: "class",
+  content: [
+    "./app/**/*.{js,ts,jsx,tsx}",
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+
+    // Or if using `src` directory:
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        burtons: "burtons",
+      },
+      animation: {
+        // Bounces 5 times 1s equals 5 seconds
+        bounce: "bounce 1s ease-in-out 2",
+      },
+    },
+  },
+  plugins: [],
+};
