a9c6030792eec85eb1d2442c0293df7fe5eb0ee0
Youssef Albokl <88749793+Boklino@users.noreply.github.com>
2023-02-08 20:24:44 +0200

    Made dark mode the default mode

--- pages/index.tsx
+++ pages/index.tsx
@@ -14,7 +14,7 @@ import { useState } from 'react'
 
 
 export default function Home() {
-  const [darkMode,setDarkMode] = useState(false)
+  const [darkMode,setDarkMode] = useState(true)
   console.log("dark",darkMode)
   return (
     <div className={darkMode ? "dark" : ""}>
